667af554800328111cd889c3f6266890ba1b33e3
Anushka <anushkamar1102@gmail.com>
2023-12-25 11:48:59 +0530

    created a ride class for initialising multiple rides

---
+++ src/main/java/Ride.java
@@ -0,0 +1,17 @@
+public class Ride {
+    private int distance;
+    private int time;
+
+    public Ride(int distance, int time) {
+        this.distance = distance;
+        this.time = time;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public int getTime() {
+        return time;
+    }
+}
